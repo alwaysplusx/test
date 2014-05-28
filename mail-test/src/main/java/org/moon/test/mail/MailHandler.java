b99d38b75cf5fa108d11e0a404cad2d877830d98
superwuxin <870757543@qq.com>
2014-05-28 11:23:23 +0800

    mail-test copy from tommy

---
+++ mail-test/src/main/java/org/moon/test/mail/MailHandler.java
@@ -0,0 +1,22 @@
+package org.moon.test.mail;
+
+public interface MailHandler {
+	/**
+	 * 发送文本格式邮件
+	 * @param mail
+	 * @return
+	 */
+	public boolean sendTextMain(Mail mail);
+	/**
+	 * 发送模版邮件
+	 * @param mail
+	 * @return
+	 */
+	public boolean sendHtmlTempletMail(Mail mail);
+	/**
+	 * 发送普通HTML邮件
+	 * @param mail
+	 * @return
+	 */
+	public boolean sendHtmlMail(Mail mail);
+}
---
+++ mail-test/src/test/java/org/moon/test/mail/MailHandlerTest.java
@@ -0,0 +1,18 @@
+package org.moon.test.mail;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.moon.test.mail.impl.MailHandlerImpl;
+
+public class MailHandlerTest {
+
+	@Test
+	public void testSendTextMain() {
+		MailHandler mailHandler = new MailHandlerImpl();
+		SimpleMail mail = new SimpleMail("tomeejee@126.com", new String[]{"870757543@qq.com"},"Come On","I Support You");
+		mail.setAttachFileNames(new String[]{"D:/material.pdf"});
+		assertEquals(true, mailHandler.sendTextMain(mail));
+	}
+
+}
