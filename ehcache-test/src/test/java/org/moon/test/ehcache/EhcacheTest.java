fd3c7998d7adc76be56834589689dfc3e70e6b7b
superwuxin <870757543@qq.com>
2014-06-27 17:31:17 +0800

    测试代码

--- ehcache-test/src/test/java/org/moon/test/ehcache/EhcacheTest.java
+++ ehcache-test/src/test/java/org/moon/test/ehcache/EhcacheTest.java
@@ -2,14 +2,17 @@ package org.moon.test.ehcache;
 
 import net.sf.ehcache.Cache;
 import net.sf.ehcache.CacheManager;
-import net.sf.ehcache.config.CacheConfiguration;
+import net.sf.ehcache.Element;
 
 public class EhcacheTest {
 
-	public static void main(String[] args) {
-		CacheManager cm = CacheManager.create();
-		cm.addCache(new Cache(new CacheConfiguration("cxfClientService", 10000)));
-		System.out.println(cm.getCacheNames()[0]);
+	public static void main(String[] args) throws Exception {
+		Cache cache = new Cache("testCache",1,false,false,1,1);
+		CacheManager manager = CacheManager.create();
+		manager.addCache(cache);
+		cache.put(new Element("abc", "def"));
+		Thread.sleep(1000 * 5);
+		System.out.println(cache.get("abc"));
 	}
 
 }
