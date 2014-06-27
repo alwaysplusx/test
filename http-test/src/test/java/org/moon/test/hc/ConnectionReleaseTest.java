964c2ee39795507ea1ad9abe6e053903eb2e18c2
superwuxin <870757543@qq.com>
2014-06-27 22:23:14 +0800

    添加文件夹

--- http-test/src/test/java/org/moon/test/hc/ConnectionReleaseTest.java
+++ http-test/src/test/java/org/moon/test/hc/ConnectionReleaseTest.java
@@ -1,6 +1,6 @@
 package org.moon.test.hc;
 
-import org.apache.http.HttpEntity;
+import org.apache.http.Header;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -11,11 +11,15 @@ public class ConnectionReleaseTest {
 	public static void main(String[] args) throws Exception {
 		CloseableHttpClient client = HttpClients.createDefault();
 		CloseableHttpResponse response = client.execute(new HttpGet("http://www.baidu.com"));
-		System.out.println(response.getStatusLine());
+		Header[] headers = response.getHeaders("");
+		for(Header header:headers){
+			System.out.println("==>"+header);
+		}
+		/*System.out.println(response.getStatusLine());
 		HttpEntity entity = response.getEntity();
 		System.out.println(entity.getContentType());
 		System.out.println(entity.getContentEncoding());
-		System.out.println(entity.getContentLength());
+		System.out.println(entity.getContentLength());*/
 	}
 	
 }
