555498c924f99a7678ccddaedd5bca4882a4020b
superwuxin <870757543@qq.com>
2014-07-20 17:26:05 +0800

    complex timer

---
+++ timer-parent/complex-timer-test/src/main/java/org/moon/test/timer/spring/SimpleService.java
@@ -0,0 +1,16 @@
+package org.moon.test.timer.spring;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SimpleService {
+
+	static Logger log = LoggerFactory.getLogger(SimpleService.class);
+
+	public void doBusiness() {
+		log.info("simple do business logic");
+	}
+
+}
--- timer-parent/complex-timer-test/src/main/java/org/moon/test/timer/spring/quartz/SimpleQuartzJob.java
+++ timer-parent/complex-timer-test/src/main/java/org/moon/test/timer/spring/quartz/SimpleQuartzJob.java
@@ -1,23 +1,32 @@
 package org.moon.test.timer.spring.quartz;
 
-import org.quartz.Job;
+import org.moon.test.timer.spring.SimpleService;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+import org.springframework.stereotype.Service;
 
-public class SimpleQuartzJob implements Job {
+@Service
+public class SimpleQuartzJob extends QuartzJobBean {
 
 	static Logger LOG = LoggerFactory.getLogger(SimpleQuartzJob.class);
-
 	@Autowired
-	private ApplicationContext context;
+	private SimpleService simpleService;
 	
 	@Override
-	public void execute(JobExecutionContext context) throws JobExecutionException {
-		LOG.info("has execute, context is " + context);
+	protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+		LOG.info("has execute, service is " + simpleService);
+	}
+
+	public SimpleService getSimpleService() {
+		return simpleService;
+	}
+
+	public void setSimpleService(SimpleService simpleService) {
+		this.simpleService = simpleService;
 	}
 
 }
