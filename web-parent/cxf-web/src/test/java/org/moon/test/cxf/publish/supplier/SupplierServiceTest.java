6ac5adf973a42b4a3e8b15ca07e5039048a033f8
superwuxin <870757543@qq.com>
2014-07-22 16:11:06 +0800

    CXF测试 客户端超时设置

--- web-parent/cxf-web/src/test/java/org/moon/test/cxf/publish/supplier/SupplierServiceTest.java
+++ web-parent/cxf-web/src/test/java/org/moon/test/cxf/publish/supplier/SupplierServiceTest.java
@@ -1,12 +1,15 @@
 package org.moon.test.cxf.publish.supplier;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.cxf.endpoint.Client;
 import org.apache.cxf.endpoint.Server;
+import org.apache.cxf.frontend.ClientProxy;
 import org.apache.cxf.jaxws.JaxWsServerFactoryBean;
+import org.apache.cxf.transport.http.HTTPConduit;
+import org.apache.cxf.transports.http.configuration.HTTPClientPolicy;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -25,6 +28,11 @@ public class SupplierServiceTest {
 		serverFactory.setServiceBean(new SupplierService() {
 			@Override
 			public boolean saveOrUpdate(List<Supplier> suppliers) {
+				try {
+					Thread.sleep(1000 * 1);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
 				return true;
 			}
 		});
@@ -38,7 +46,14 @@ public class SupplierServiceTest {
 
 	@Test
 	public void testSupplierSaveOrUpdate() throws Exception {
-		assertTrue(ProxyFactoryManager.create(SupplierService.class, address).saveOrUpdate(new ArrayList<Supplier>()));
+		SupplierService client = ProxyFactoryManager.create(SupplierService.class, address);
+		Client clientProxy = ClientProxy.getClient(client);
+		HTTPConduit conduit = (HTTPConduit)clientProxy.getConduit();
+		HTTPClientPolicy policy = new HTTPClientPolicy();
+		policy.setReceiveTimeout(1000 * 65);
+		policy.setConnectionTimeout(1000 * 65);
+		conduit.setClient(policy);
+		assertTrue(client.saveOrUpdate(null));
 	}
 
 }
