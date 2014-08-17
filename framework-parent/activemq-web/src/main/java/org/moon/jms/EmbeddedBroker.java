1cb1dad3336855dbbbfae75c3688b24be9ac7b66
wuxii <870757543@qq.com>
2014-08-17 23:13:16 +0800

    Signed-off-by: wuxii <870757543@qq.com>

---
+++ framework-parent/activemq-web/src/main/java/org/moon/jms/EmbeddedBroker.java
@@ -0,0 +1,22 @@
+package org.moon.jms;
+
+import org.apache.activemq.broker.BrokerService;
+import org.apache.activemq.store.memory.MemoryPersistenceAdapter;
+
+public final class EmbeddedBroker {
+	private EmbeddedBroker() {
+	}
+
+	public static void main(String[] args) throws Exception {
+		BrokerService broker = new BrokerService();
+		broker.setPersistenceAdapter(new MemoryPersistenceAdapter());
+		broker.setDataDirectory("target/activemq-data");
+		broker.addConnector("tcp://localhost:61616");
+		broker.start();
+		System.out.println("JMS broker ready ...");
+		Thread.sleep(125 * 60 * 1000);
+		System.out.println("JMS broker exiting");
+		broker.stop();
+		System.exit(0);
+	}
+}
