904cc8ca84fe839b462dc095218b2d95a6344f3a
wuxii <wuxii@foxmail.com>
2014-11-28 19:57:44 +0800

    log4j

---
+++ logger-parent/log4j-test/src/test/java/org/moon/test/log4j/LoggerPrinter.java
@@ -0,0 +1,14 @@
+package org.moon.test.log4j;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoggerPrinter {
+
+	static final Logger log = LoggerFactory.getLogger(LoggerPrinter.class);
+
+	public static void main(String[] args) {
+		log.info("abc");
+	}
+
+}
