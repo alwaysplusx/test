12bd1aca0af53e68f39d707c8afaf2068b8c3085
wuxii <wuxii@foxmail.com>
2014-12-02 00:00:24 +0800

    jaxrs simple core

---
+++ framework-parent/jaxrs-web/src/main/java/org/moon/test/jaxrs/Library.java
@@ -0,0 +1,15 @@
+package org.moon.test.jaxrs;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("/library")
+public class Library {
+
+	@GET
+	@Path("/books")
+	public String books() {
+		return "more and more";
+	}
+
+}
