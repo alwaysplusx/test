cf8744a0e36d58953b4aac09cbde8d0c8a85bddd
wuxii <wuxii@foxmail.com>
2015-04-19 16:15:11 +0800

    dozer test
    Signed-off-by: wuxii <wuxii@foxmail.com>

---
+++ basic-parent/dozer-test/src/test/java/org/moon/test/mapper/vo/B.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2013-2015 wuxii@foxmail.com.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.moon.test.mapper.vo;
+
+/**
+ * @author wuxii@foxmail.com
+ */
+public class B extends VoSupport {
+
+	private static final long serialVersionUID = 1L;
+
+	private C c;
+
+	public C getC() {
+		return c;
+	}
+
+	public void setC(C c) {
+		this.c = c;
+	}
+
+	@Override
+	public String toString() {
+		return getClass().getName() + ":{\"c\":\"" + c + "\"}";
+	}
+
+}
