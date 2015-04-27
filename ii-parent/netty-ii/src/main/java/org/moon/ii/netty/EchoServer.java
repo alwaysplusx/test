e5fcf2d6bf13b5db5cfeaf4be016f1b7fbbad4af
wuxii <wuxii@foxmail.com>
2015-04-27 23:36:13 +0800

    修改pom.xml相关路径为父路径下的文件， 添加netty测试工程
    Signed-off-by: wuxii <wuxii@foxmail.com>

---
+++ ii-parent/netty-ii/src/main/java/org/moon/ii/netty/EchoServer.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2013-2015 wuxii@foxmail.com.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.moon.ii.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * @author wuxii@foxmail.com
+ */
+public class EchoServer {
+
+	private int port = 8080;
+
+	public static void main(String[] args) throws Exception {
+		new EchoServer().run();
+	}
+
+	public void run() throws Exception {
+		EventLoopGroup bossGroup = new NioEventLoopGroup(); // (1)
+		EventLoopGroup workerGroup = new NioEventLoopGroup();
+		try {
+			ServerBootstrap b = new ServerBootstrap(); // (2)
+			b.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class) // (3)
+					.childHandler(new ChannelInitializer<SocketChannel>() { // (4)
+								@Override
+								public void initChannel(SocketChannel ch) throws Exception {
+									ch.pipeline().addLast(new EchoServerHandler());
+								}
+							}).option(ChannelOption.SO_BACKLOG, 128) // (5)
+					.childOption(ChannelOption.SO_KEEPALIVE, true); // (6)
+
+			// Bind and start to accept incoming connections.
+			ChannelFuture f = b.bind(port).sync(); // (7)
+
+			// Wait until the server socket is closed.
+			// In this example, this does not happen, but you can do that to
+			// gracefully
+			// shut down your server.
+			f.channel().closeFuture().sync();
+		} finally {
+			workerGroup.shutdownGracefully();
+			bossGroup.shutdownGracefully();
+		}
+	}
+
+}
