3caad2e2e3e3324f03db073d9c71067040459e7f
wuxii <wuxii@foxmail.com>
2015-05-04 10:31:07 +0800

    wait and notify test
    Signed-off-by: wuxii <wuxii@foxmail.com>

---
+++ ii-parent/concurrent-ii/src/test/java/org/moon/ii/concurrent/WaitNotifyTest.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2002-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.moon.ii.concurrent;
+
+import org.junit.Test;
+
+/**
+ * @author wuxii@foxmail.com
+ */
+public class WaitNotifyTest {
+
+    private static final Object lockObject = new Object();
+    private int waitTime = 1000 * 10;
+    private boolean stop;
+    private int deep;
+
+    @Test
+    public void test() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (lockObject) {
+                    loop();
+                }
+            }
+        }).start();
+        // main thread
+        long start = System.currentTimeMillis();
+        try {
+            synchronized (lockObject) {
+                while (!stop && System.currentTimeMillis() - start < waitTime) {
+                    lockObject.notify();
+                    lockObject.wait();
+                    System.out.println(">>> main thread be notified");
+                }
+                stop = true;
+            }
+        } catch (InterruptedException e) {
+        }
+    }
+
+    public void loop() {
+        deep++;
+        System.out.println(">> loop of " + deep);
+        lockObject.notify();
+        try {
+            if (stop) {
+                return;
+            }
+            lockObject.wait();
+            System.out.println(">> sun thread be notified");
+            Thread.sleep(1000);
+            loop();
+        } catch (Exception e) {
+        } finally {
+            deep--;
+        }
+    }
+
+}
