e64a7dc38cef58ab191b24d8f2fcb2bab6d1a918
wuxii <wuxii@foxmail.com>
2018-05-12 00:23:45 +0800

    update

--- spring/eureka/eureka-client-movie/src/main/java/org/harmony/eureka/client/RecommendationApplication.java
+++ spring/eureka/eureka-client-movie/src/main/java/org/harmony/eureka/client/RecommendationApplication.java
@@ -12,18 +12,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 /**
  * @author wuxii@foxmail.com
  */
 @EnableDiscoveryClient
+@EnableHystrix
 @SpringBootApplication
 @RestController
 @RequestMapping("/api/movie")
@@ -47,7 +50,7 @@ public class RecommendationApplication {
             new Movie("hook", 0));
 
     @GetMapping("/recommendations/{user}")
-    @ResponseBody
+    @HystrixCommand(fallbackMethod = "fallback")
     public List<Movie> recommendations(@PathVariable("user") String user) {
         Member member = restTemplate.getForObject("http://membership/api/member/{user}", Member.class, user);
         if (member == null) {
@@ -55,6 +58,11 @@ public class RecommendationApplication {
         }
         int age = member.getAge();
         return movies.stream().filter(e -> e.getLimit() < age).collect(Collectors.toList());
+    }
+
+    // 容错方法， enable hystrix，方法参数要与被容错的方法相同
+    public List<Movie> fallback(String user) {
+        return movies.stream().filter(e -> e.getLimit() == 0).collect(Collectors.toList());
     }
 
 }
--- test/test-javase/test-javase-java9/src/main/java/org/harmony/test/java9/hello/Main.java
+++
@@ -1,7 +0,0 @@
-package org.harmony.test.java9.hello;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.printf("Hello World");
-    }
-}
