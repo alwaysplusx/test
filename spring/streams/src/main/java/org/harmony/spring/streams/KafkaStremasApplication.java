47baf788db16a47d5ac2fc2bb2b18d1ec49dbe92
wuxii <wuxii@foxmail.com>
2018-06-11 23:31:13 +0800

    add kafka streams

---
+++ spring/streams/src/main/java/org/harmony/spring/streams/KafkaStremasApplication.java
@@ -0,0 +1,36 @@
+package org.harmony.spring.streams;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wuxii@foxmail.com
+ */
+@RestController
+@SpringBootApplication
+public class KafkaStremasApplication {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(KafkaStremasApplication.class, args);
+    }
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @RequestMapping("/")
+    public String echo() {
+        kafkaTemplate.send("foo", "foo");
+        return "{\"Hello\":\"World\"}";
+    }
+
+    @KafkaListener(topics = "foo", groupId = "foo")
+    public void handleMessage(String msg) {
+        System.out.println("receive message -> " + msg);
+    }
+
+}
